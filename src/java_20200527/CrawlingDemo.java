d1d07345985c73f67d117dc03ab4e24e4d5a2e40
theblue9 <cjh930310@gmail.com>
2020-05-27 18:05:42 +0900

    2020-05-27-v1

--- src/java_20200527/CrawlingDemo.java
+++ src/java_20200527/CrawlingDemo.java
@@ -51,18 +51,26 @@ public class CrawlingDemo {
 
 	public static void main(String[] args) {
 		/*
-		 * // TODO Auto-generated method stub String url = "http://www.imbc.com/";
+		 * // TODO Auto-generated method stub String 
+		 * url = "http://www.imbc.com/";
 		 * 
-		 * //HTML 문서 전체를 관리하기 위한 객체 Document doc = null;
+		 * //HTML 문서 전체를 관리하기 위한 객체 
+		 * Document doc = null;
 		 * 
-		 * try { //url로 연결하여 html 정보를 get 방식으로 가져온다. doc = Jsoup.connect(url).get(); }
-		 * catch (IOException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
+		 * try { 
+		 * //url로 연결하여 html 정보를 get 방식으로 가져온다. 
+		 * doc = Jsoup.connect(url).get(); }
+		 * catch (IOException e) { 
+		 * // TODO Auto-generated catch block
+		 * e.printStackTrace(); 
+		 * }
 		 * 
-		 * System.out.println("성공..."); //html 정보에서 class 정보를 가져올때는 . id 정보를 가져올때는 #
+		 * System.out.println("성공..."); 
+		 * //html 정보에서 class 정보를 가져올때는 . id 정보를 가져올때는 #
 		 * Elements elements = doc.select(".notice-list li");
 		 * 
-		 * for(int i=0; i<elements.size();i++) { Element liElement = elements.get(i);
+		 * for(int i=0; i<elements.size();i++) { 
+		 * Element liElement = elements.get(i);
 		 * System.out.println(liElement.text()); }
 		 */
 
